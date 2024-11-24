97f711fec9e7348f8be7d26e468f1039223c62af
pierre-juarez <76144339+pierre-juarez@users.noreply.github.com>
2024-11-24 00:30:47 -0500

    [feat] - Implementation of NetworkManager to handle HTTP requests

---
+++ DBZ-App/Networking/NetworkManager.swift
@@ -0,0 +1,54 @@
+//
+//  NetworkManager.swift
+//  DBZ-App
+//
+//  Created by Jhonatan Chavez on 24/11/24.
+//
+
+import Foundation
+
+enum NetworkError: Error{
+    case invalidURL
+    case noData
+}
+
+class NetworkManager{
+    
+    static let shared = NetworkManager()
+    private let baseURL = "https://dragonball-api.com/api"
+    
+    func fetchData<T: Decodable>(endpoint: String, responseType: T.Type, completion: @escaping(Result<T, Error>) -> Void){
+        
+        guard let url = URL(string: "\(baseURL)\(endpoint)") else{
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url){data, response, error in
+            
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else{
+                completion(.failure(NetworkError.noData))
+                return
+            }
+            
+            do{
+                let decoder = JSONDecoder()
+                let decodedData = try decoder.decode(T.self, from: data)
+                completion(.success(decodedData))
+            }catch{
+                completion(.failure(error))
+            }
+            
+        }
+        
+        task.resume()
+        
+    }
+    
+    
+}
